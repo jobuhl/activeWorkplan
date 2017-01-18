7f83b303e4613fb09d84f0ac3b1530d1adb675a3
urskenner <urskenner@web.de>
2017-01-18 23:53:58 +0100

    Forget Password mit header und Footer + Contact mit Fehler meldung nach button click

--- resources/views/admin/employer-nostore.blade.php
+++ resources/views/admin/employer-nostore.blade.php
@@ -1,15 +1,32 @@
-<!DOCTYPE html>
-<html lang="en">
+@extends('admin.layout.employer-start')
 
-<head>
+@section('css')
+    <link rel="stylesheet" type="text/css" href="{{asset('css/employer/no-store.css')}}">
+@endsection
 
-</head>
+@section('content')
+
+    <div class="fake-body">
+
+        <section class="container horizontal">
+
+            <div class="row">
+
+                <h2 class="modal-ueberschrift">At the moment your Company has no Stores.</h2>
+                <br>
+                <br>
+                <div class="col-xs-12 col-sm-4 col-sm-offset-4">
+                    <button class="form-control add-button" type="submit" data-toggle="modal" data-target="#add-button-store">
+                        Add Store
+                    </button>
+                </div>
+
+            </div>
 
 
-<body>
-    <p> no store</p>
+        </section>
 
-</body>
+        @include('admin.includes.add-store')
+    </div>
 
-
-</html>
+@endsection
