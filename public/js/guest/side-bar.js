b193a7b924cfc9124ad1a2741baef6f7c4626005
alexanderschmutz <alexander.schmutz@htwg-konstanz.de>
2017-02-13 21:50:48 +0100

    colors renamed

--- public/js/guest/side-bar.js
+++ public/js/guest/side-bar.js
@@ -1,17 +1,12 @@
 /* Wenn Seite neu geladen */
 window.onload = function () {
-    /* Trennlinie nur berechnen, wenn darstellung "list" */
-    if (!$("#aside-overview").hasClass("kachel")) {
+    /* Trennlinie nur berechnen */
         sideBarBorder();
-    }
 };
 
 /* Wenn Fenster vergroessert oder verkleinert wird */
 $('body')[0].onresize = function () {
-    /* Trennlinie nur berechnen, wenn darstellung "list" */
-    if (!$("#aside-overview").hasClass("kachel")) {
         sideBarBorder();
-    }
 };
 
 function sideBarBorder() {
@@ -28,10 +23,10 @@ function sideBarBorder() {
 
         if (sideBar > rightSide) {
             /* Wenn Side-bar hoeher: An rechte Seite der Side-bar Trennlinie zum aside setzen */
-            $(".side-bar").css("border-right", "1px solid var(--main-border-color)");
+            $(".side-bar").css("border-right", "1px solid var(--grey-border)");
         } else {
             /* Wenn rechtes aside hoeher: An linke Seite des aside Trennlinie zur side-bar setzen */
-            $(".my-right-side").css("border-left", "1px solid var(--main-border-color)");
+            $(".my-right-side").css("border-left", "1px solid var(--grey-border)");
         }
     } else {
         /* Wenn Mobil: keine border */
